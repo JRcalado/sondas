e4e38aec36ac4d49e66ce03a776907f067598d57
Ricardo Calado <jricardoc10@gmail.com>
2021-09-29 08:43:07 -0300

    Criando testes para os updates da sonda

--- test/structs/sonda_test.exs
+++ test/structs/sonda_test.exs
@@ -6,4 +6,14 @@ defmodule Sondas.SondaTest do
   test "create new sonda" do
     assert Sonda.new([1, 2], "n") == %Sondas.Structs.Sonda{orientation: "n", position: [1, 2]}
   end
+
+  test "update sonda orientation" do
+    sonda1 = Sonda.new([1, 2], "n")
+    assert Sonda.update_orientation(sonda1, "e") == %Sondas.Structs.Sonda{orientation: "e", position: [1, 2]}
+  end
+
+  test "update sonda position" do
+    sonda1 = Sonda.new([1, 2], "n")
+    assert Sonda.update_position(sonda1, [5, 4]) == %Sondas.Structs.Sonda{orientation: "n", position: [5, 4]}
+  end
 end
