1cd702ce9bd5b3529a63c6be81228a66d9633292
Ricardo Calado <jricardoc10@gmail.com>
2021-09-28 23:36:50 -0300

    criando struct da sondas

--- lib/sondas.ex
+++ lib/sondas.ex
@@ -1,17 +1,4 @@
 defmodule Sondas do
-  @moduledoc """
-  Documentation for `Sondas`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Sondas.hello()
-      :world
-
-  """
   def hello do
     :world
   end
