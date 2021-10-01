7c65f88c5c3821336e25dbca97abb382358c3e92
Ricardo Calado <jricardoc10@gmail.com>
2021-10-01 09:20:39 -0300

    Criando agent para as sondas

--- lib/services/moving_sonda.ex
+++ lib/services/moving_sonda.ex
@@ -1,24 +1,55 @@
 defmodule Sondas.Services.MovingSonda do
 
+  alias Sondas.Agents.SondaAgent
+  alias Sondas.Structs.Sonda
+
   def action_sonda(sonda, grid, command) do
     #  IO.inspect(grid)
     #  IO.inspect(sonda)
     #  String.graphemes(command)
 
-     String.graphemes(command)
-     |> Enum.each(fn args -> control(args, sonda)|>IO.inspect end)
+    key="sonda1"
+    SondaAgent.start_link
+    SondaAgent.set(sonda,key)
+
+    String.graphemes(command)
+     |> Enum.each(
+        fn args -> control(args,  SondaAgent.get(key), key) |> IO.inspect
+        end
+      )
+      SondaAgent.get(key)
   end
 
+  def updade_orientatio(orientation, key) do
 
+    SondaAgent.get(key)
+    |> Sonda.update_orientation(orientation)
+    |> SondaAgent.set('sonda1')
 
-  def control(out, sonda)do
+    SondaAgent.get('sonda1')
+
+  end
+
+  def updade_position(position, key) do
+
+    SondaAgent.get(key)
+    |> Sonda.update_position(position)
+    |> SondaAgent.set('sonda1')
+
+    SondaAgent.get('sonda1')
+
+  end
+
+  def control(out, sonda, key)do
 
 
      action = String.upcase(out)
 
     cond do
-      action == "L" or action == "R" -> calculate(action,  sonda.orientation )
+      action == "L" or action == "R" -> calculate(action,  sonda.orientation)
+                                            |> updade_orientatio(key)
       action == "M" -> moved( sonda.position,  sonda.orientation)
+                                            |> updade_position(key)
       true -> {:error, "coordenadas erradas"}
 
     end
